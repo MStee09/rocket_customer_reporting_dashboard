a762541a22a4478d5cab3630c27a8ede8eeeff4e
MStee09 <michael@gorocketshipping.com>
2026-01-10 10:32:00 -0600

    Added index.ts

---
+++ src/KTimIzIYIx5BjSHJ/index.ts
@@ -0,0 +1,50 @@
+export type {
+  VisualizationType,
+  GeoMapKey,
+  VisualizationConfig,
+  FilterOperator,
+  FilterBlock,
+  AILogicBlock,
+  CompiledRule,
+  LogicBlock,
+  PublishConfig,
+  VisualBuilderSchema,
+  ValidationResult,
+} from './types/BuilderSchema';
+
+export {
+  createDefaultBuilderSchema,
+  validateBuilderSchema,
+} from './types/BuilderSchema';
+
+export {
+  compileLogicBlocks,
+  applyFiltersToQuery,
+  createFilterBlock,
+  createAILogicBlock,
+  serializeLogicBlocks,
+  deserializeLogicBlocks,
+} from './logic/compileLogic';
+
+export {
+  compileAILogic,
+  parseSimpleLogic,
+  updateAIBlockWithCompilation,
+  AVAILABLE_FIELDS,
+} from './logic/aiCompilation';
+
+export {
+  BuilderProvider,
+  useBuilder,
+  loadDraftFromStorage,
+  clearDraftFromStorage,
+} from './components/BuilderContext';
+
+// Main entry point - using UnifiedVisualBuilderPage
+export { UnifiedVisualBuilderPage as VisualBuilderPage } from './components/UnifiedVisualBuilderPage';
+
+export { VisualizationPanel } from './components/panels/VisualizationPanel';
+export { FieldMappingPanel } from './components/panels/FieldMappingPanel';
+export { LogicPanel } from './components/panels/LogicPanel';
+export { PreviewPanel } from './components/panels/PreviewPanel';
+export { PublishPanel } from './components/panels/PublishPanel';

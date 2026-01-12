19409a542110017a6d08c9a5785faca9a12f8d4f
MStee09 <michael@gorocketshipping.com>
2026-01-11 21:10:17 -0600

    Updated index.ts

--- src/admin/visual-builder/index.ts
+++ src/admin/visual-builder/index.ts
@@ -43,7 +43,7 @@ export {
   clearDraftFromStorage,
 } from './components/BuilderContext';
 
-export { VisualBuilderV5Final as VisualBuilderPage } from './components/VisualBuilderV5Final';
+export { VisualBuilderV5Working as VisualBuilderPage } from './components/VisualBuilderV5Working';
 export { CustomerScopeSelector } from './components/CustomerScopeSelector';
 export { ChartTypeSelector } from './components/ChartTypeSelector';
 export { DateRangeDisplay } from './components/DateRangeDisplay';

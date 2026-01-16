4796ecce36bfbea78706505f4450bbf9b4434321
MStee09 <michael@gorocketshipping.com>
2026-01-16 13:53:19 -0600

    Deleted AccountNotesSection.tsx

--- src/components/knowledge-base/profile-editor/index.ts
+++
@@ -1,4 +0,0 @@
-export { TerminologySection } from './TerminologySection';
-export { MarketsSection } from './MarketsSection';
-export { BenchmarkSection } from './BenchmarkSection';
-export { AccountNotesSection } from './AccountNotesSection';
